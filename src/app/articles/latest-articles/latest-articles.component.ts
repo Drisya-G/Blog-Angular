387dfd021073c830a10c7de936507bac3518c6a5
Drisya-G <111036377+Drisya-G@users.noreply.github.com>
2023-01-14 15:27:37 +0530

    Blog App

---
+++ src/app/articles/latest-articles/latest-articles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-latest-articles',
+  templateUrl: './latest-articles.component.html',
+  styleUrls: ['./latest-articles.component.css']
+})
+export class LatestArticlesComponent {
+
+}
